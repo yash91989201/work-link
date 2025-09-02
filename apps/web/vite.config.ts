bfd8b2009a87a5c31e6aaa9342bfdd81a58a70e8
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-09-02 22:27:19 +0530

    update vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,30 +1,31 @@
-import { VitePWA } from "vite-plugin-pwa";
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		tanstackRouter({}),
-		react(),
-		VitePWA({
-			registerType: "autoUpdate",
-			manifest: {
-				name: "work-link",
-				short_name: "work-link",
-				description: "work-link - PWA Application",
-				theme_color: "#0c0c0c",
-			},
-			pwaAssets: { disabled: false, config: true },
-			devOptions: { enabled: true },
-		}),
-	],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [
+    tailwindcss(),
+    tanstackRouter({}),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Work Link",
+        short_name: "work-link",
+        description:
+          "Work Link is a lightweight, white-label team collaboration platform with secure channels, user management, and real-time communication tools.",
+        theme_color: "#0c0c0c",
+      },
+      pwaAssets: { disabled: false, config: true },
+      devOptions: { enabled: true },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
