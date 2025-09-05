992f2fa75145199af7037835bddee6fe87ef2f40
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-09-05 11:01:12 +0530

    stage UI component refactors
    Signed-off-by: Yashraj Jaiswal <yashraj6370@gmail.com>

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,32 +1,34 @@
 import { Link } from "@tanstack/react-router";
 import { ModeToggle } from "./mode-toggle";
 import UserMenu from "./user-menu";
+import { buttonVariants } from "./ui/button";
 
 export default function Header() {
-	const links = [
-		{ to: "/", label: "Home" },
-		{ to: "/dashboard", label: "Dashboard" },
-		{ to: "/todos", label: "Todos" },
-	] as const;
-
-	return (
-		<div>
-			<div className="flex flex-row items-center justify-between px-2 py-1">
-				<nav className="flex gap-4 text-lg">
-					{links.map(({ to, label }) => {
-						return (
-							<Link key={to} to={to}>
-								{label}
-							</Link>
-						);
-					})}
-				</nav>
-				<div className="flex items-center gap-2">
-					<ModeToggle />
-					<UserMenu />
-				</div>
-			</div>
-			<hr />
-		</div>
-	);
+  return (
+    <div>
+      <nav className="container mx-auto flex flex-row items-center justify-between px-2 py-1">
+        <Link className="flex items-center gap-2" to="/">
+          <picture>
+            <source srcSet="/logo.webp" type="image/webp" />
+            <img
+              alt="Logo"
+              height={60}
+              loading="lazy"
+              src="/logo.webp"
+              width={60}
+            />
+          </picture>
+          <h1 className="font-semibold text-2xl">Work Link</h1>
+        </Link>
+        <div className="flex items-center gap-2">
+          <Link to="/signin" className={buttonVariants({ variant: "outline" })}>
+            Sign In
+          </Link>
+          <ModeToggle />
+          <UserMenu />
+        </div>
+      </nav>
+      <hr />
+    </div>
+  );
 }
