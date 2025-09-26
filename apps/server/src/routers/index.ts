9e99310986b196fd24d64bf73a249724de656dd0
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-09-26 10:17:19 +0530

    remove org router for now

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -1,12 +1,11 @@
 import type { RouterClient } from "@orpc/server";
 import { publicProcedure } from "@/lib/orpc";
-import { organizationRotuer } from "./organization";
 
 export const appRouter = {
   healthCheck: publicProcedure.handler(() => {
     return "OK";
   }),
-  organization: organizationRotuer,
 };
+
 export type AppRouter = typeof appRouter;
 export type AppRouterClient = RouterClient<typeof appRouter>;
