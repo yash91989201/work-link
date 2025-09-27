251ab91ab02001e283faf1eec15a124d02242d31
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-09-28 00:29:42 +0530

    refactor(routing): remove obsolete route files
    - remove old admin route layout file
    - remove old team routes using $slug parameter
    - clean up unused module and feature route components
    - prepare for new route structure with $id parameters

--- apps/web/src/routes/(authenticated)/org/$slug/(member)/team/$slug/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/(authenticated)/org/$slug/(member)/team/$slug/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/(authenticated)/org/$slug/team/$slug/"!</div>
-}
