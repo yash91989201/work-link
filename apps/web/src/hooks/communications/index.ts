3a6f4461656fdd4cd39b9013f44f1790e9f26c1a
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-10-10 20:06:27 +0530

    chore: remove deprecated communication components and hooks
    - remove old member search filter
    - remove old attendance greeting
    - remove old message composer and list components
    - remove deprecated communication hooks
    - remove old route file

--- apps/web/src/hooks/communications/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./use-attachments";
-export * from "./use-channels";
-// Export refactored hooks by default
-export * from "./use-messages-refactored";
-// Keep old exports for backward compatibility
-export { useMessage as useMessageOld } from "./use-messages-old";
-export * from "./use-notifications";
