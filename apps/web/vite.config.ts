e75d04bf4920cd8762aa75126abe7189ee50f1c1
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-10-12 21:50:21 +0530

    update manual chunking

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          react: ["react", "react-dom", "scheduler", "react/jsx-runtime"],
+          react: ["react", "react-dom"],
           tanstack: [
             "@tanstack/react-query",
             "@tanstack/react-router",
